7ef9874009c5311d801ea79b6a3907cce26b940d
Sandra Michalska <sandra.kinga.michalska@gmail.com>
2023-11-24 09:22:44 +0100

    CSS code improvements

--- js/start.js
+++ js/start.js
@@ -8,7 +8,7 @@ export let gameLogic = null;
 export function init() {
 	renderer = new Renderer();
 	
-	document.querySelector('#settings__start-btn').addEventListener('click', function() {
+	document.querySelector('#settings__start-button').addEventListener('click', function() {
 		const settings = prepareSettings();
 		toggleSections();
 
@@ -37,16 +37,16 @@ function prepareSettings() {
 }
 
 function toggleSections() {
-	document.querySelector('#game-wrapper').classList.remove("game-wrapper--hidden");
-	document.querySelector('#settings-wrapper').classList.add("settings-wrapper--hidden");
+	document.querySelector('#game').classList.remove("game--hidden");
+	document.querySelector('#settings').classList.add("settings--hidden");
 	document.querySelector('#header__back-arrow').classList.remove("header__back-arrow--hidden");
 
 	document.querySelector('#header__back-arrow').addEventListener('click', function() {
 		clearTimeout(gameLogic.loopGameTimeout);
 		gameLogic.resetGameValues();
 		document.querySelector('#game__score-list').innerHTML = '';
-		document.querySelector('#game-wrapper').classList.add("game-wrapper--hidden");
-		document.querySelector('#settings-wrapper').classList.remove("settings-wrapper--hidden");
+		document.querySelector('#game').classList.add("game--hidden");
+		document.querySelector('#settings').classList.remove("settings--hidden");
 		document.querySelector('#header__back-arrow').classList.add("header__back-arrow--hidden");
 	});
 }
--- main.js
+++ main.js
@@ -9,12 +9,12 @@ document.querySelector('#app').innerHTML = `
         <h1 class="header__heading">Snake</h1>
       </div>
     </header>
-    <section id="settings-wrapper" class="settings-wrapper">
-      <fieldset class="settings">
+    <section id="settings" class="settings">
+      <fieldset class="settings__fieldset">
         <legend class="settings__title">Settings</legend>
 
         <section class="settings__option">
-          <label class="settings__option-label" for="level">Level</label>
+          <label class="settings__label" for="level">Level</label>
           <select id="level" class="settings__select">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -24,7 +24,7 @@ document.querySelector('#app').innerHTML = `
           </select>
         </section>
         <section class="settings__option">
-          <label class="settings__option-label" for="speed">Snake speed</label>
+          <label class="settings__label" for="speed">Snake speed</label>
           <select id="speed" class="settings__select">
             <option value="1">slow</option>
             <option value="2">medium</option>
@@ -32,7 +32,7 @@ document.querySelector('#app').innerHTML = `
           </select>
         </section>
         <section class="settings__option">
-          <label class="settings__option-label" for="speed-change-time">Snake speed change after catching a powerup</label>
+          <label class="settings__label" for="speed-change-time">Snake speed change after catching a powerup</label>
           <select id="speed-change-time" class="settings__select">
             <option value="1">1 second</option>
             <option value="2">2 seconds</option>
@@ -40,7 +40,7 @@ document.querySelector('#app').innerHTML = `
           </select>
         </section>
         <section class="settings__option">
-          <label class="settings__option-label" for="length-change">Snake length change after catching a powerup</label>
+          <label class="settings__label" for="length-change">Snake length change after catching a powerup</label>
           <select id="length-change" class="settings__select">
             <option value="1">1 cell</option>
             <option value="2">2 cells</option>
@@ -48,25 +48,23 @@ document.querySelector('#app').innerHTML = `
           </select>
         </section>
 
-        <div class="settings__start-btn-wrapper">
-          <button type="button" id="settings__start-btn" class="settings__start-btn">Start</button>
+        <div class="settings__button-wrapper">
+          <button type="button" id="settings__start-button" class="settings__start-button">Start</button>
         </div>
       </fieldset>
     </section>
 
-    <div id="game-wrapper" class="game-wrapper game-wrapper--hidden">
-      <div class="game">
+    <section id="game" class="game game--hidden">
         <section>
-          <p class="game__score-txt">Score: <span id="game__score">0</span></p>
+          <p class="game__score-text">Score: <span id="game__score">0</span></p>
           <canvas id="canvas" class="game__canvas" height="601px" width="601px"></canvas>
         </section>
 
         <section class="game__best-scores">
-          <p class="game__best-score-txt">Best scores:</p>
+          <p class="game__best-scores-text">Best scores:</p>
           <ol id="game__score-list" class="game__score-list"></ol>
         </section>
-      </div>
-    </div>
+    </section>
   </div>
 `
 
